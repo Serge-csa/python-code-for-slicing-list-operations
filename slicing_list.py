7ff4aee04e6ebc975818978829f8e37627b18bc7
Serge-csa <2025839676.ambokwe@ug.sharda.ac.in>
2026-02-04 21:17:02 +0100

    Add slicing examples for lists in slicing_list.py
    list slicing is a technique use in python to extract a specific section of a sequence, such as a list, tuple or string

---
+++ slicing_list.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sliced_list = my_list[2:5]
+print(sliced_list)
+
+#negative indices
+sliced_list = my_list[-4:-1]
+print(sliced_list)
+
+#slicing with a specified step
+sliced_list = my_list[1:9:2]
+print(sliced_list)
